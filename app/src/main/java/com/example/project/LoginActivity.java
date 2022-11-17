ed4fb7ffb70bb32b0743f1f6eb00c8b2c28750d0
ihernandezvelarde <irene.hernandezihv@gmail.com>
2022-11-17 22:38:48 +0100

    Revert "//S1 P9. //Añadir Cliente. (No funciona, mirar errores)"
    This reverts commit bbd77f6f4db19c15364ebad2b77a6963a1a03293.

--- app/src/main/java/com/example/project/LoginActivity.java
+++ app/src/main/java/com/example/project/LoginActivity.java
@@ -2,47 +2,23 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-import java.util.HashMap;
-
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    int port = 8888;
-    String location = "10.0.2.2";
-    String uri = "ws://" + location + ":" + port;
-    Client socket = new Client();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        final EditText server = findViewById(R.id.inputID);
-        final EditText user = findViewById(R.id.inputUsernameLogin);
-        final EditText password = findViewById(R.id.inputPasswordLogin);
-        final Button loginButton = findViewById(R.id.btnLogin);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        TextView btn = findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                socket.conectando();
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                HashMap<String, String> users = socket.getUsers();
-                for (String key : users.keySet()) {
-                    if (user.getText().toString().equals(key) && password.getText().toString().equals(users.get(user.getText().toString()))) {
-                        Toast.makeText(LoginActivity.this, "LOGIN CORRECTE", Toast.LENGTH_SHORT).show();
-                    }
-                }
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
             }
         });
-
     }
 }
-
-
